d772cac10c2e6a5437f18d668585e58b659f7fa0
Turubarova Elizaveta <turubarova@sibers.com>
2017-07-10 20:31:07 -0400

    Password is now repeated type, form controls in password form are neat

--- src/AppBundle/Form/CustomerPasswordType.php
+++ src/AppBundle/Form/CustomerPasswordType.php
@@ -3,6 +3,8 @@
 namespace AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,8 +15,14 @@ class CustomerPasswordType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder
-            ->add('password');
+        $builder->add('password', RepeatedType::class, array(
+            'type' => PasswordType::class,
+            'invalid_message' => 'The password fields must match.',
+            'options' => array('attr' => array('class' => 'password-field form-control')),
+            'required' => true,
+            'first_options'  => array('label' => 'Password'),
+            'second_options' => array('label' => 'Repeat Password'),
+        ));
     }
     
     /**
